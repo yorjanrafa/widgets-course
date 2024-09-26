fee80b7715e79925f70ce95e686fdc95820d4c3b
Sandoval <yorjan1999sandoval@gmail.com>
2024-09-26 19:17:31 -0400

    Screens created

---
+++ lib/presentation/screens/infinite_scroll_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class InfiniteScroll extends StatelessWidget {
+  static const String name = 'infinite_screen';
+
+  const InfiniteScroll({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
---
+++ lib/presentation/screens/snackbar/snackbar_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class snackbarScreen extends StatelessWidget {
+  static const String name = 'snackbar_screen';
+
+  const snackbarScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
